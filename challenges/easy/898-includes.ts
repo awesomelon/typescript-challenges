91b65fce939af8029c2d4a0e4351cc9cb6ae0550
Jaeho Bang <jhb@marpple.com>
2024-05-28 11:19:18 +0900

    3057 push.ts

--- challenges/easy/898-includes.ts
+++ challenges/easy/898-includes.ts
@@ -18,33 +18,54 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
+export type Equal3<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
+    ? true
+    : false;
+
+type MyEqual1<A, B> = A extends B ? (B extends A ? true : false) : false;
+
+type MyEqual2<T extends readonly any[], U> = {
+  [K in keyof T]: MyEqual2<T[K], U>;
+} extends false[]
+  ? false
+  : true;
+
 type Includes<T extends readonly any[], U> = T extends [infer F, ...infer Rest]
-    ? F extends U
-        ? true
-        : Includes<Rest, U>
-    : false
+  ? MyEqual1<F, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false;
+
+// type Includes<T extends readonly any[], U> = {
+//     [P in T[number]]: true
+// }[U] extends true ? true : false;
 
 /* _____________ 테스트 케이스 _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
-    Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Kars'>, true>>,
-    Expect<Equal<Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>, false>>,
-    Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
-    Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
-    Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
-    Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
-    Expect<Equal<Includes<[{}], { a: 'A' }>, false>>,
-    Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
-    Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
-    Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
-    Expect<Equal<Includes<[{ a: 'A' }], { readonly a: 'A' }>, false>>,
-    Expect<Equal<Includes<[{ readonly a: 'A' }], { a: 'A' }>, false>>,
-    Expect<Equal<Includes<[1], 1 | 2>, false>>,
-    Expect<Equal<Includes<[1 | 2], 1>, false>>,
-    Expect<Equal<Includes<[null], undefined>, false>>,
-    Expect<Equal<Includes<[undefined], null>, false>>,
-]
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">, true>
+  >,
+  Expect<
+    Equal<Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">, false>
+  >,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 7>, true>>,
+  Expect<Equal<Includes<[1, 2, 3, 5, 6, 7], 4>, false>>,
+  Expect<Equal<Includes<[1, 2, 3], 2>, true>>,
+  Expect<Equal<Includes<[1, 2, 3], 1>, true>>,
+  Expect<Equal<Includes<[{}], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[boolean, 2, 3, 5, 6, 7], false>, false>>,
+  Expect<Equal<Includes<[true, 2, 3, 5, 6, 7], boolean>, false>>,
+  Expect<Equal<Includes<[false, 2, 3, 5, 6, 7], false>, true>>,
+  Expect<Equal<Includes<[{ a: "A" }], { readonly a: "A" }>, false>>,
+  Expect<Equal<Includes<[{ readonly a: "A" }], { a: "A" }>, false>>,
+  Expect<Equal<Includes<[1], 1 | 2>, false>>,
+  Expect<Equal<Includes<[1 | 2], 1>, false>>,
+  Expect<Equal<Includes<[null], undefined>, false>>,
+  Expect<Equal<Includes<[undefined], null>, false>>,
+];
 
 /* _____________ 다음 단계 _____________ */
 /*
